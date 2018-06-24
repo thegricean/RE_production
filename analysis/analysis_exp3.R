8be43bc06b34befc7083dd851b9d32504a52565e
thegricean <judithdegen@gmail.com>
2018-06-24 13:18:15 -0700

    updated exp 3 analysis with brm and new main results plot

--- analysis/analysis_exp3.R
+++ analysis/analysis_exp3.R
@@ -1,4 +1,8 @@
 library(tidyverse)
+library(languageR)
+library(lme4)
+library(MuMIn)
+library(brms)
 theme_set(theme_bw(18))
 source("helper_scripts/helpers.R")
 source("helper_scripts/createLaTeXTable.R")
@@ -10,10 +14,11 @@ nrow(d)
 
 # Add costs
 costs = read.table(file="../data/cost_exp3.csv",sep=",", header=T, quote="")
+row.names(costs) = as.character(costs$target)
 
-d$freq_sub = costs[as.character(d$target_sub),]$relFreq
-d$freq_basic = costs[as.character(d$target_basic),]$relFreq
-d$freq_super = costs[as.character(d$target_super),]$relFreq
+d$freq_sub = costs[as.character(d$target_sub),]$freq
+d$freq_basic = costs[as.character(d$target_basic),]$freq
+d$freq_super = costs[as.character(d$target_super),]$freq
 
 d$log_freq_sub = log(d$freq_sub)
 d$log_freq_basic = log(d$freq_basic)
@@ -22,13 +27,12 @@ d$log_freq_super = log(d$freq_super)
 d$diff_logfreq_subbasic = d$log_freq_sub - d$log_freq_basic
 d$diff_logfreq_subsuper = d$log_freq_sub - d$log_freq_super
 
-row.names(costs) = costs$noun
-costs$precodedLength = nchar(as.character(costs$noun))
+costs$precodedLength = nchar(as.character(costs$target))
 
 # empirical
-d$mean_length_sub = costs[as.character(d$target_sub),]$average_length
-d$mean_length_basic =  costs[as.character(d$target_basic),]$average_length
-d$mean_length_super =  costs[as.character(d$target_super),]$average_length
+d$mean_length_sub = costs[as.character(d$target_sub),]$length
+d$mean_length_basic =  costs[as.character(d$target_basic),]$length
+d$mean_length_super =  costs[as.character(d$target_super),]$length
 
 # precoded
 d$precoded_length_sub = costs[as.character(d$target_sub),]$precodedLength
@@ -39,8 +43,8 @@ d$ratio_length_subbasic = d$mean_length_sub/d$mean_length_basic
 d$ratio_length_subsuper = d$mean_length_sub/d$mean_length_super
 
 # get length/frequency correlation
-costs$logFreq = log(costs$relFreq)
-cor(costs$logFreq,costs$average_length) # -.44
+costs$logFreq = log(costs$freq)
+cor(costs$logFreq,costs$length) # -.44
 
 
 # Add typicality values
@@ -80,9 +84,6 @@ centered = cbind(d, myCenter(d[,c("mean_length_sub","mean_length_basic","mean_le
 contrasts(centered$condition) = cbind("12.vs.rest"=c(3/4,-1/4,-1/4,-1/4),"22.vs.3"=c(0,2/3,-1/3,-1/3),"23.vs.33"=c(0,0,1/2,-1/2))
 contrasts(centered$redCondition) = cbind("sub.vs.rest"=c(-1/3,2/3,-1/3),"basic.vs.super"=c(1/2,0,-1/2))
 
-library(languageR)
-library(lme4)
-
 pairscor.fnc(centered[,c("cdiff_logfreq_subbasic","cratio_length_subbasic","cratio_typ_subbasic","redCondition","sub")])
 
 m.1 = glmer(sub ~ condition + (1|gameid) + (1|target_sub), family="binomial", data=centered)
@@ -110,14 +111,14 @@ m = glmer(sub ~ redCondition + cdiff_logfreq_subbasic + cratio_length_subbasic +
 summary(m) # frequency doesn't appear to matter one bit
 createLatexTable(m,predictornames=c("Intercept","Condition sub.vs.rest","Condition basic.vs.super","Length","Frequency","Length:Frequency"))
 
-# add typicality -- this is what's reported in the psych review paper
+# add typicality -- main analysis reported in the psych review paper
 m.m.t = glmer(sub ~ redCondition + cdiff_logfreq_subbasic + cratio_length_subbasic + cratio_typ_subbasic + (1|gameid) + (1|target_sub) , family="binomial",data=centered) 
 summary(m.m.t)
 createLatexTable(m.m.t,predictornames=c("Intercept","Condition sub.vs.rest","Condition basic.vs.super","Length","Frequency","Typicality"))
 
 anova(m,m.m.t) # typicality very important!
 
-# add interaction term -- it does nothing
+# add length/frequency interaction term -- it does nothing
 m.m.t.inter = glmer(sub ~ redCondition + cdiff_logfreq_subbasic + cratio_length_subbasic + cdiff_logfreq_subbasic:cratio_length_subbasic + cratio_typ_subbasic + (1|gameid) + (1|target_sub) , family="binomial",data=centered) 
 summary(m.m.t.inter)
 
@@ -133,8 +134,7 @@ summary(m.m.t.c)
 
 anova(m.m.t,m.m.t.c) # nope, no value added by more complex condition
 
-library(MuMIn)
-
+# get marginal and conditional R squared
 r.squaredGLMM(m)
 r.squaredGLMM(m.m.t) # increase of .06 in marginal R2, so typicality certainly explains variance
 r.squaredGLMM(m.m.t.c) # further increase in explained variance with most complex condition close to 0
@@ -153,11 +153,41 @@ empirical$PredictionM = ifelse(empirical$FittedM >= .5, T, F)
 cor(empirical$sub,empirical$Prediction)
 cor(empirical$sub,empirical$PredictionM) # better correlation with than without random effects
 
+# do a brms analysis with more conservative (ie, fuller) random effects structure (reported in paper in footnote)
+m.m.t.b = brm(sub ~ redCondition + cdiff_logfreq_subbasic + cratio_length_subbasic + cratio_typ_subbasic + (1+redCondition + cdiff_logfreq_subbasic + cratio_length_subbasic + cratio_typ_subbasic|gameid) + (1+redCondition + cdiff_logfreq_subbasic + cratio_length_subbasic + cratio_typ_subbasic|target_sub) , family="bernoulli",data=centered) 
+summary(m.m.t.b)
 
+mean(posterior_samples(m.m.t.b, pars = "b_redCondition") > 0)
+mean(posterior_samples(m.m.t.b, pars = "b_cratio_typ_subbasic") > 0)
+mean(posterior_samples(m.m.t.b, pars = "b_cratio_length_subbasic") < 0)
 
 ######### PAPER PLOTS
 
-# overall pattern, fig ??
+# overall pattern, fig 18
+agr = d %>%
+  select(redCondition,target_sub,typ_sub,sub,basic,super) %>%
+  gather(Utt,Mentioned,-redCondition,-target_sub,-typ_sub) %>%
+  group_by(Utt,redCondition,target_sub,typ_sub) %>%
+  summarise(Probability=mean(Mentioned),ci.low=ci.low(Mentioned),ci.high=ci.high(Mentioned)) %>%
+  ungroup() %>%
+  mutate(YMin=Probability-ci.low,YMax=Probability+ci.high) %>%
+  mutate(redCondition=fct_recode(redCondition,"sub necessary"="sub_necessary","basic sufficient"="basic_sufficient","super sufficient"="super_sufficient"),Utterance=fct_relevel(Utt,c("sub","basic","super")))
+agr$item = ""
+agr[agr$target_sub == "parrot",]$item = "parrot"
+agr[agr$target_sub == "poloshirt",]$item = "poloshirt"
+agr[agr$target_sub == "suv",]$item = "suv"
+
+ggplot(agr, aes(x=typ_sub,y=Probability,color=Utterance)) +
+  geom_point() +
+  geom_smooth(method="lm") +
+  geom_text(aes(label=item),nudge_y=.04) +
+  ylab("Empirical utterance proportion") +
+  xlab("Typicality of object for subordinate level utterance") +
+  facet_wrap(~fct_relevel(redCondition,c("sub necessary","basic sufficient","super sufficient"))) +
+  theme(legend.position="top")
+ggsave("../writing/pics/results-exp3.png",height=4.5,width=9.5)
+
+# overall pattern, collapsed
 agr = d %>%
   select(sub,basic,super, condition) %>%
   gather(Utt,Mentioned,-condition) %>%
@@ -177,117 +207,14 @@ ggplot(agr, aes(x=condition,y=Probability)) +
   facet_wrap(~Utterance) +
   ylab("Proportion of utterance choice") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))
-#ggsave("graphs_basiclevel/results-collapsed.pdf",height=4.1,width=7)
 
-agr = d %>%
-  select(sub,basic,super, condition, target_basic) %>%
-  gather(Utt,Mentioned,-condition, -target_basic) %>%
-  group_by(Utt,condition,target_basic) %>%
-  summarise(Probability=mean(Mentioned),ci.low=ci.low(Mentioned),ci.high=ci.high(Mentioned))
-agr = as.data.frame(agr)
-agr$YMin = agr$Probability - agr$ci.low
-agr$YMax = agr$Probability + agr$ci.high
-summary(agr)
-dodge = position_dodge(.9)
-agr$Utt = as.factor(ifelse(agr$Utt == "sub","sub",ifelse(agr$Utt == "basic","basic","super")))
-agr$Utterance = factor(x=as.character(agr$Utt),levels=c("sub","basic","super"))
-
-ggplot(agr, aes(x=condition,y=Probability)) +
-  geom_bar(stat="identity",position=dodge) +
-  geom_errorbar(aes(ymin=YMin,ymax=YMax),width=.25, position=dodge) +
-  facet_grid(target_basic~Utterance) +
-  ylab("Proportion of utterance choice") +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))
-#ggsave("graphs_basiclevel/results-bydomain.pdf",height=10,width=7)
 
 # correlation between mean empirical length and ratio of sub to basic length
 cor(d$mean_length_sub,d$ratio_length_subbasic) # r=.84 between mean length and sub to basic ratio
 cor(d$mean_length_sub,d$ratio_length_subsuper) # r=.83 between mean length and sub to super ratio
 
-#main effect of length
-d$bin_ratio_length_subbasic = cut_number(d$ratio_length_subbasic,2,labels=c("short","long"))#,labels=c("short","mid","long"))
 
-summary(cut_number(d$ratio_length_subbasic,2))
-           
-agr = d %>%
-  select(sub, bin_ratio_length_subbasic,redCondition) %>%
-  group_by(bin_ratio_length_subbasic,redCondition) %>%
-  summarise(Probability=mean(sub),ci.low=ci.low(sub),ci.high=ci.high(sub))
-agr = as.data.frame(agr)
-agr$YMin = agr$Probability - agr$ci.low
-agr$YMax = agr$Probability + agr$ci.high
-summary(agr)
-dodge = position_dodge(.9)
-agr$Condition = factor(x=gsub("_","\n",as.character(agr$redCondition)),levels=c("sub\nnecessary","basic\nsufficient","super\nsufficient"))
-
-library(wesanderson)
-
-pl = ggplot(agr, aes(x=bin_ratio_length_subbasic,y=Probability)) +
-  geom_bar(stat="identity",position=dodge,color="black") +
-  geom_errorbar(aes(ymin=YMin,ymax=YMax),width=.25, position=dodge) +
-  scale_x_discrete(name="Sub level length",breaks=c("short","long"),labels=c("short\n ","long\n ")) +
-  scale_fill_manual(values=wes_palette("Darjeeling2"),name="Length") +
-  scale_y_continuous(name="Proportion of sub level mention",breaks=seq(0,1,.2)) +
-  facet_wrap(~Condition) +
-  theme(axis.title.y = element_blank(),axis.title.x = element_text(size=10),axis.text.y = element_text(size=8),plot.margin=unit(c(0,0,0,0.2), "cm"))
-#theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))
-#ggsave("/Users/titlis/cogsci/projects/stanford/projects/overinformativeness/writing/2016/cogsci/graphs/length-effect.pdf",height=4.2,width=6)
-
-# main effect of typicality
-d$bin_typ_subbasic = cut_number(d$ratio_typ_subbasic,2,labels=c("less typical","more typical"))#,labels=c("short","mid","long"))
-
-summary(cut_number(d$ratio_typ_subbasic,2))
-
-agr = d %>%
-  select(sub, bin_typ_subbasic,redCondition) %>%
-  group_by(bin_typ_subbasic,redCondition) %>%
-  summarise(Probability=mean(sub),ci.low=ci.low(sub),ci.high=ci.high(sub))
-agr = as.data.frame(agr)
-agr$YMin = agr$Probability - agr$ci.low
-agr$YMax = agr$Probability + agr$ci.high
-summary(agr)
-dodge = position_dodge(.9)
-agr$Condition = factor(x=gsub("_","\n",as.character(agr$redCondition)),levels=c("sub\nnecessary","basic\nsufficient","super\nsufficient"))
-#agr$Condition = factor(x=as.character(agr$redCondition),levels=c("sub_necessary","basic_sufficient","super_sufficient"))
-agr$Typicality = factor(x=as.character(agr$bin_typ_subbasic),levels=c("more typical","less typical"))
-
-pt = ggplot(agr, aes(x=Typicality,y=Probability)) +
-  geom_bar(stat="identity",position=dodge,color="black") +
-  geom_errorbar(aes(ymin=YMin,ymax=YMax),width=.25, position=dodge) +
-  scale_x_discrete(name="Sub level typicality",breaks=c("more typical","less typical"),labels=c("more\ntypical","less\ntypical")) +
-  #scale_fill_manual(values=wes_palette("Darjeeling2"),name="Length") +
-  scale_y_continuous(name="Proportion of sub level mention",breaks=seq(0,1,.2)) +
-  facet_wrap(~Condition) +
-  theme(axis.title.y = element_blank(),axis.title.x = element_text(size=10),axis.text.y = element_text(size=8),plot.margin=unit(c(0,0,0,0.2), "cm"))
-
-library(gridExtra)
-library(grid)
-pdf("../writing/pics/lengthtypicality.pdf",height=3.5,width=8)
-grid.arrange(pl,pt,nrow=1,  left = textGrob("Proportion of sub level mention", rot = 90, vjust = 1,gp = gpar(cex = .9)))
-dev.off()
-
-
-######### LOOK AT QUALITATIVE CASES
-grizzly = droplevels(d[d$target_sub == "grizzlybear",])
-grizzly$koala_alt = as.factor(ifelse(grizzly$alt1_sub == "koalabear" | grizzly$alt2_sub == "koalabear",1,0))
-table(grizzly$koala_alt,grizzly$basic,grizzly$condition)
-
-panda = droplevels(d[d$target_sub == "pandabear",])
-table(panda$condition,panda$sub)
-
-hummingbird = droplevels(d[d$target_sub == "hummingbird",])
-table(hummingbird$condition,hummingbird$sub)
-
-pug = droplevels(d[d$target_sub == "pug",])
-table(pug$condition,pug$sub)
-13/44 # = .295, proportion of sub level mentions in basic and super sufficient conditions
-
-gs = droplevels(d[d$target_sub == "germanshepherd",])
-table(gs$condition,gs$sub)
-4/35 # = .11, proportion of sub level mentions in basic and super sufficient conditions
-
-
-
+### CONTINUE UPDATING HERE
 
 ##### Modeling plots (this code was originally in writing/rscripts/plots.R)
 
