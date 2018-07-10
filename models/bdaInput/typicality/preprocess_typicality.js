29bbbb4c3b20e2246f426ed03056010c5ec5f437
Robert Hawkins <rxdh@stanford.edu>
2018-07-10 13:02:29 -0700

    Update raw input for typicality

--- models/bdaInput/typicality/preprocess_typicality.js
+++ models/bdaInput/typicality/preprocess_typicality.js
@@ -1,17 +1,17 @@
 var fs = require('fs');
 var _ = require('lodash');
-var refModule = require('../refModule');
+var refModule = require('../../refModule');
 
 // Convert data to json
 // TODO: use raw data from ../../data/ here
 // TODO: make sure original actually has a header...
-var rawData = refModule.readCSV("./bda_data.csv"); 
+var rawData = refModule.readCSV("./bda_data-raw.csv"); 
 fs.writeFileSync('./bda_data.json',
 		 JSON.stringify(rawData, null, 2));
 
 // Convert conditions to json with unified context field
 // TODO: generate this from rawData by _.distinct()? 
-var rawConditions = refModule.readCSV("./unique_conditions.csv"); 
+var rawConditions = refModule.readCSV("./unique_conditions-raw.csv"); 
 var conditions = _.map(rawConditions, function(c) {
   return _.extend(c, {context: [
     {color: c.t_color, item: c.t_item},
@@ -22,5 +22,5 @@ var conditions = _.map(rawConditions, function(c) {
 
 console.log(conditions);
 fs.writeFileSync('./unique_conditions.json',
-		 JSON.stringify(rawData, null, 2));
+		 JSON.stringify(conditions, null, 2));
 console.log("Constructing contexts complete...");
