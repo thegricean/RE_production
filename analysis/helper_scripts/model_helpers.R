dcb4c3c081f7aa8fd712a47d4166a9adea7c5421
Robert Hawkins <rxdh@stanford.edu>
2018-07-11 00:08:32 -0700

    Add helper functions for model analysis notebok

---
+++ analysis/helper_scripts/model_helpers.R
@@ -0,0 +1,14 @@
+
+se <- function(x)
+{
+  y <- x[!is.na(x)] # remove the missing values, if any
+  sqrt(var(as.vector(y))/length(y))
+}
+
+## for bootstrapping 95% confidence intervals
+library(bootstrap)
+theta <- function(x,xdata,na.rm=T) {mean(xdata[x],na.rm=na.rm)}
+ci.low <- function(x,na.rm=T) {
+  mean(x,na.rm=na.rm) - quantile(bootstrap(1:length(x),1000,theta,x,na.rm=na.rm)$thetastar,.025,na.rm=na.rm)}
+ci.high <- function(x,na.rm=T) {
+  quantile(bootstrap(1:length(x),1000,theta,x,na.rm=na.rm)$thetastar,.975,na.rm=na.rm) - mean(x,na.rm=na.rm)}
