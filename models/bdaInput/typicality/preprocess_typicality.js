2629981c7766612b4095e552222838c8a07f5107
Robert Hawkins <rxdh@stanford.edu>
2018-08-27 18:30:46 -0700

    Add context info to typicality

--- models/bdaInput/typicality/preprocess_typicality.js
+++ models/bdaInput/typicality/preprocess_typicality.js
@@ -1,25 +1,30 @@
 var fs = require('fs');
 var _ = require('lodash');
-var refModule = require('../../refModule');
+var babyparse = require('babyparse');
 
-// Convert data to json
-// TODO: use raw data from ../../data/ here
-// TODO: make sure original actually has a header...
-var rawData = refModule.readCSV("./bda_data-raw.csv"); 
-fs.writeFileSync('./bda_data.json',
-		 JSON.stringify(rawData, null, 2));
+function readCSV(filename){
+  return babyparse.parse(fs.readFileSync(filename, 'utf8'),
+			 {header:true, keepEmptyRows:false,     skipEmptyLines: true}).data;
+};
 
-// Convert conditions to json with unified context field
-// TODO: generate this from rawData by _.distinct()? 
-var rawConditions = refModule.readCSV("./unique_conditions-raw.csv"); 
-var conditions = _.map(rawConditions, function(c) {
+var getConditions = function(c) {
   return _.extend(c, {context: [
     {color: c.t_color, item: c.t_item},
     {color: c.d1_color, item: c.d1_item},
     {color: c.d2_color, item: c.d2_item}
   ]});
-});
+};
 
+// Convert data to json
+var rawData = readCSV("./bda_data.csv");
+var data = _.map(rawData, getConditions);
+fs.writeFileSync('./bda_data.json',
+		 JSON.stringify(data, null, 2));
+
+// Convert conditions to json with unified context field
+// TODO: generate this from rawData by _.distinct()? 
+var rawConditions = readCSV("./unique_conditions.csv"); 
+var conditions = _.map(rawConditions, getConditions);
 console.log(conditions);
 fs.writeFileSync('./unique_conditions.json',
 		 JSON.stringify(conditions, null, 2));
