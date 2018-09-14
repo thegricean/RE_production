d7f0022b6bb83352b7970a088a08bbf0d1844567
Robert X.D. Hawkins <rxdh@stanford.edu>
2018-09-14 13:45:36 -0400

    Update runbatch_BF.sh

--- models/runbatch_BF.sh
+++ models/runbatch_BF.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Need to use gnu parallel to limit number of these running at once (for memory reasons)
-# find ../bdaInput/*.csv | parallel -j 4 --bar "sh runbatch_BF.sh" {}
+# parallel -j+0 'sh runbatch_BF.sh' ::: {1..10} 
 # We sleep a bit to prevent writing at the same time...
 
 # Run with file input
