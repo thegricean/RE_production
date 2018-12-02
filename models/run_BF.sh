cb2bd29f701a87a66da5e1c285005f22b7697bbb
Robert Hawkins <hawkrobe@gmail.com>
2018-12-01 18:07:19 -0800

    refactor BF with input

--- models/run_BF.sh
+++ models/run_BF.sh
@@ -1,3 +1,3 @@
 #!/bin/bash#
-# parallel --bar --colsep ',' "sh ./run_BF.sh {1} {2} {3}" :::: BF_input.txt  > "BF_output.txt"
+# parallel --bar --colsep ',' "sh ./run_BF.sh {1} {2} {3}" :::: BFInput/BF_nominal.txt  > "BFOutput/BF_nominal.txt"
 webppl BF.wppl --require ./refModule/ -- --modelVersion $1 --costs $2 --semantics $3
